a7a37ceb0c76b43ddb061f5caf9b49c10689a6df
Courtney Lippman <courtney.lippman@colorado.edu>
2023-01-14 14:17:15 -0700

    Build Favorites component and style it.

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,17 +1,30 @@
 import './Favorites.css'
+import FavBookCard from '../FavBookCard/FavBookCard'
 
 
-const Favorites = () => {
+const Favorites = ({ pairingList }) => {
+    const favPairingsList = pairingList.reduce((acc, pairing) => {
+        if(pairing.isSaved) {
+            acc.push(
+                <FavBookCard
+                key={pairing.book.title}
+                title={pairing.book.title}
+                image={pairing.book.bookImg}
+                drink={pairing.drink.name}
+                isSaved={pairing.isSaved}
+                />
+            )
+        }
+        return acc
+    }, [])
+
     return (
         <div className='favorites'>
-
+            <h1 className='favorites-page-title'>The Favorites List</h1>
+            {favPairingsList.length === 0 && <h1 className="no-fav-message">You currently have no favorite games. Go and favorite some cool games!</h1>}
+            <div className='favorites-card-wrapper'>{favPairingsList}</div>
         </div>
     )
 }
-
-
-
-
-
 
 export default Favorites
