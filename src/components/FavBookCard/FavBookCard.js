e88940782f36aa14e765ca627684e953e3330102
Courtney Lippman <courtney.lippman@colorado.edu>
2023-01-16 13:07:49 -0700

    Add details tests WIP.

--- src/components/FavBookCard/FavBookCard.js
+++ src/components/FavBookCard/FavBookCard.js
@@ -5,12 +5,13 @@ import PropTypes from 'prop-types'
 
 const FavBookCard = ({ title, image, drink, toggleSavePairing, drinkId }) => {
     const navigate = useNavigate()
-    const handleClick = () => {
+    const handleClick = (event) => {
+        event.preventDefault()
       navigate(`/details/${title}  ${drinkId}`)
     }
     return (
         <div className='fav-book-card'>
-            <button onClick={handleClick} className="fav-book-card">
+            <button onClick={handleClick} className="fav-book-card" id='fav-book-button'>
                 <img style={{display: !image &&  'none'}} src={image} alt={"Book Cover of " + title } />
                 <p className='fav-drink-name'>{drink}</p>
             </button>
